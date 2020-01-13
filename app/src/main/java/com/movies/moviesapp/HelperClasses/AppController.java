9b92a451574fb6e5ef310cf6c8049ead9c6eeb83
Sonu <sonubabu@cocoalabs.in>
2020-01-13 17:02:07 +0530

    Commit listing and detail page

--- app/src/main/java/com/movies/moviesapp/HelperClasses/AppController.java
+++ app/src/main/java/com/movies/moviesapp/HelperClasses/AppController.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 public class AppController extends Application {
     private static AppController mInstance;
+
     @Override
     public void onCreate() {
         super.onCreate();
